1d9575f1e59fb418955997d708c059df4e1b850c
zam0ng <sinze27@gmail.com>
2023-09-08 17:19:19 +0900

    1

--- front/src/Axios.js
+++ front/src/Axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // axios.defaults.baseURL = 'http://3.37.244.154:8080/api';    // aws 에서 탄력적 ip 주소 넣음
 // axios.defaults.baseURL = 'http://3.37.244.154:8080';    // aws 에서 탄력적 ip 주소 넣음 || aws 배포용 🚀
 // axios.defaults.baseURL = 'http://localhost:8080';   // 로컬 테스트용 🔵
-axios.defaults.baseURL = 'https://www.jaeyeong.site';   // 로컬 테스트용 🔵
+axios.defaults.baseURL = 'https://www.jaeyeong.site:8080';   // 로컬 테스트용 🔵
 
 
 // ⭐ axios 뽑아서 사용할 때 withCredentials : true 항상 적용하는 법 ⭐
